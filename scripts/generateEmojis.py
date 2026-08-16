399f1bcb4ebfe6f3c4447411c91c7a00f028edb5
Henri Sendelbach <henri.sendelbach@thehsi.cloud>
2026-08-17 00:26:45 +0300

    Updated Emojis.java to have every emoji be @NotNull
    Also made the generateEmojis.py use the correct paths

--- scripts/generateEmojis.py
+++ scripts/generateEmojis.py
@@ -9,16 +9,17 @@ with open("emojis", 'r') as f:
 
 n = 0
 
-with open("Emojis.java", 'w') as f:
+with open("../comitas-api/src/main/java/cloud/thehsi/ComitasBotJ/API/Discord/Emoji/Emojis/Emojis.java", 'w') as f:
     f.write("""package cloud.thehsi.ComitasBotJ.API.Discord.Emoji.Emojis;
     
 import cloud.thehsi.ComitasBotJ.API.Discord.Emoji.Emoji;
+import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings({"unused", "SpellCheckingInspection"})
 public class Emojis {\n""")
 
 for i in range(0, len(data), 500):
-    with open("IEmojisData" + str(n) + ".java", 'w') as f:
+    with open("../comitas-api/src/main/java/cloud/thehsi/ComitasBotJ/API/Discord/Emoji/Emojis/IEmojisData" + str(n) + ".java", 'w') as f:
         f.write("""package cloud.thehsi.ComitasBotJ.API.Discord.Emoji.Emojis;
 
 import cloud.thehsi.ComitasBotJ.API.Discord.Emoji.Emoji;
@@ -29,10 +30,12 @@ class IEmojisData""" + str(n) + " {\n")
             f.write("    static final Emoji " + elm[0] + " = Emoji.fromUnicode(" + elm[1] + ");\n")
         f.write("}")
     
-    with open("Emojis.java", 'a') as f:
+    with open("../comitas-api/src/main/java/cloud/thehsi/ComitasBotJ/API/Discord/Emoji/Emojis/Emojis.java", 'a') as f:
             for elm in data[i:i+500]:
-                f.write("    public static final Emoji " + elm[0] + " = IEmojisData" + str(n) + "." + elm[0] + ";\n")
+                f.write("    @NotNull public static final Emoji " + elm[0] + " = IEmojisData" + str(n) + "." + elm[0] + ";\n")
     n+=1
 
-with open("Emojis.java", 'a') as f:
+with open("../comitas-api/src/main/java/cloud/thehsi/ComitasBotJ/API/Discord/Emoji/Emojis/Emojis.java", 'a') as f:
     f.write("}")
+
+print("Generated Emojis")
