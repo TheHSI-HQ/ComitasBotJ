09c1df30d50b6f840fc483e788f3a868012fa8a9
Tessa Sendelbach <tessa.sendelbach@thehsi.cloud>
2026-08-20 01:25:46 +0300

    Fixed bug in Permission.java, with permissions having improper ids

--- comitas-api/src/main/java/cloud/thehsi/ComitasBotJ/API/Discord/Permission.java
+++ comitas-api/src/main/java/cloud/thehsi/ComitasBotJ/API/Discord/Permission.java
@@ -3,178 +3,88 @@ package cloud.thehsi.ComitasBotJ.API.Discord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@SuppressWarnings({"unused", "ClassCanBeRecord"})
-public class Permission {
-    public static final Permission CREATE_INSTANT_INVITE =
-            new Permission(1L, "CREATE_INSTANT_INVITE");
-    public static final Permission KICK_MEMBERS =
-            new Permission(2L << 1, "KICK_MEMBERS");
-    public static final Permission BAN_MEMBERS =
-            new Permission(2L << 2, "BAN_MEMBERS");
-    public static final Permission ADMINISTRATOR =
-            new Permission(2L << 3, "ADMINISTRATOR");
-    public static final Permission MANAGE_CHANNEL =
-            new Permission(2L << 4, "MANAGE_CHANNEL");
-    public static final Permission MANAGE_GUILD =
-            new Permission(2L << 5, "MANAGE_GUILD");
-    public static final Permission ADD_REACTIONS =
-            new Permission(2L << 6, "ADD_REACTIONS");
-    public static final Permission VIEW_AUDIT_LOGS =
-            new Permission(2L << 7, "VIEW_AUDIT_LOGS");
-    public static final Permission PRIORITY_SPEAKER =
-            new Permission(2L << 8, "PRIORITY_SPEAKER");
-    public static final Permission STREAM =
-            new Permission(2L << 9, "STREAM");
-    public static final Permission VIEW_CHANNEL =
-            new Permission(2L << 10, "VIEW_CHANNEL");
-    public static final Permission SEND_MESSAGES =
-            new Permission(2L << 11, "SEND_MESSAGES");
-    public static final Permission SEND_TTS_MESSAGES =
-            new Permission(2L << 12, "SEND_TTS_MESSAGES");
-    public static final Permission MANAGE_MESSAGES =
-            new Permission(2L << 13, "MANAGE_MESSAGES");
-    public static final Permission EMBED_LINKS =
-            new Permission(2L << 14, "EMBED_LINKS");
-    public static final Permission ATTACH_FILES =
-            new Permission(2L << 15, "ATTACH_FILES");
-    public static final Permission READ_MESSAGE_HISTORY =
-            new Permission(2L << 16, "READ_MESSAGE_HISTORY");
-    public static final Permission MENTION_EVERYONE =
-            new Permission(2L << 17, "MENTION_EVERYONE");
-    public static final Permission USE_EXTERNAL_EMOJIS =
-            new Permission(2L << 18, "USE_EXTERNAL_EMOJIS");
-    public static final Permission VIEW_GUILD_INSIGHTS =
-            new Permission(2L << 19, "VIEW_GUILD_INSIGHTS");
-    public static final Permission CONNECT =
-            new Permission(2L << 20, "CONNECT");
-    public static final Permission SPEAK =
-            new Permission(2L << 21, "SPEAK");
-    public static final Permission MUTE_MEMBERS =
-            new Permission(2L << 22, "MUTE_MEMBERS");
-    public static final Permission DEAFEN_MEMBERS =
-            new Permission(2L << 23, "DEAFEN_MEMBERS");
-    public static final Permission MOVE_MEMBERS =
-            new Permission(2L << 24, "MOVE_MEMBERS");
-    public static final Permission USE_VAD =
-            new Permission(2L << 25, "USE_VAD");
-    public static final Permission CHANGE_NICKNAME =
-            new Permission(2L << 26, "CHANGE_NICKNAME");
-    public static final Permission MANAGE_NICKNAMES =
-            new Permission(2L << 27, "MANAGE_NICKNAMES");
-    public static final Permission MANAGE_ROLES =
-            new Permission(2L << 28, "MANAGE_ROLES");
-    public static final Permission MANAGE_WEBHOOKS =
-            new Permission(2L << 29, "MANAGE_WEBHOOKS");
-    public static final Permission MANAGE_GUILD_EXPRESSIONS =
-            new Permission(2L << 30, "MANAGE_GUILD_EXPRESSIONS");
-    public static final Permission USE_APPLICATION_COMMANDS =
-            new Permission(2L << 31, "USE_APPLICATION_COMMANDS");
-    public static final Permission REQUEST_TO_SPEAK =
-            new Permission(2L << 32, "REQUEST_TO_SPEAK");
-    public static final Permission MANAGE_EVENTS =
-            new Permission(2L << 33, "MANAGE_EVENTS");
-    public static final Permission MANAGE_THREADS =
-            new Permission(2L << 34, "MANAGE_THREADS");
-    public static final Permission CREATE_PUBLIC_THREADS =
-            new Permission(2L << 35, "CREATE_PUBLIC_THREADS");
-    public static final Permission CREATE_PRIVATE_THREADS =
-            new Permission(2L << 36, "CREATE_PRIVATE_THREADS");
-    public static final Permission USE_EXTERNAL_STICKERS =
-            new Permission(2L << 37, "USE_EXTERNAL_STICKERS");
-    public static final Permission SEND_MESSAGES_IN_THREADS =
-            new Permission(2L << 38, "SEND_MESSAGES_IN_THREADS");
-    public static final Permission USE_EMBEDDED_ACTIVITIES =
-            new Permission(2L << 39, "USE_EMBEDDED_ACTIVITIES");
-    public static final Permission MODERATE_MEMBERS =
-            new Permission(2L << 40, "MODERATE_MEMBERS");
-    public static final Permission VIEW_CREATOR_MONETIZATION_ANALYTICS =
-            new Permission(2L << 41, "VIEW_CREATOR_MONETIZATION_ANALYTICS");
-    public static final Permission USE_SOUNDBOARD =
-            new Permission(2L << 42, "USE_SOUNDBOARD");
-    public static final Permission CREATE_GUILD_EXPRESSIONS =
-            new Permission(2L << 43, "CREATE_GUILD_EXPRESSIONS");
-    public static final Permission CREATE_EVENTS =
-            new Permission(2L << 44, "CREATE_EVENTS");
-    public static final Permission USE_EXTERNAL_SOUNDS =
-            new Permission(2L << 45, "USE_EXTERNAL_SOUNDS");
-    public static final Permission SEND_VOICE_MESSAGES =
-            new Permission(2L << 46, "SEND_VOICE_MESSAGES");
-    public static final Permission SET_VOICE_CHANNEL_STATUS =
-            new Permission(2L << 48, "SET_VOICE_CHANNEL_STATUS");
-    public static final Permission SEND_POLLS =
-            new Permission(2L << 49, "SEND_POLLS");
-    public static final Permission USE_EXTERNAL_APPS =
-            new Permission(2L << 50, "USE_EXTERNAL_APPS");
-    public static final Permission PIN_MESSAGES =
-            new Permission(2L << 51, "PIN_MESSAGES");
-    public static final Permission BYPASS_SLOWMODE =
-            new Permission(2L << 52, "BYPASS_SLOWMODE");
+import java.util.Arrays;
+
+@SuppressWarnings({"unused"})
+public enum Permission {
+    CREATE_INSTANT_INVITE(1L, "CREATE_INSTANT_INVITE"),
+    KICK_MEMBERS(2L, "KICK_MEMBERS"),
+    BAN_MEMBERS(2L << 1, "BAN_MEMBERS"),
+    ADMINISTRATOR(2L << 2, "ADMINISTRATOR"),
+    MANAGE_CHANNEL(2L << 3, "MANAGE_CHANNEL"),
+    MANAGE_GUILD(2L << 4, "MANAGE_SERVER", "MANAGE_GUILD"),
+    ADD_REACTIONS(2L << 5, "ADD_REACTIONS", "MESSAGE_ADD_REACTION"),
+    VIEW_AUDIT_LOGS(2L << 6, "VIEW_AUDIT_LOGS"),
+    PRIORITY_SPEAKER(2L << 7, "PRIORITY_SPEAKER"),
+    STREAM(2L << 8, "STREAM", "VOICE_STREAM"),
+    VIEW_CHANNEL(2L << 9, "VIEW_CHANNEL"),
+    SEND_MESSAGES(2L << 10, "SEND_MESSAGES", "MESSAGE_SEND"),
+    SEND_TTS_MESSAGES(2L << 11, "SEND_TTS_MESSAGES", "MESSAGE_TTS"),
+    MANAGE_MESSAGES(2L << 12, "MANAGE_MESSAGES", "MESSAGE_MANAGE"),
+    EMBED_LINKS(2L << 13, "EMBED_LINKS", "MESSAGE_EMBED_LINKS"),
+    ATTACH_FILES(2L << 14, "ATTACH_FILES", "MESSAGE_ATTACH_FILES"),
+    READ_MESSAGE_HISTORY(2L << 15, "READ_MESSAGE_HISTORY", "MESSAGE_HISTORY"),
+    MENTION_EVERYONE(2L << 16, "MENTION_EVERYONE", "MESSAGE_MENTION_EVERYONE"),
+    USE_EXTERNAL_EMOJIS(2L << 17, "USE_EXTERNAL_EMOJIS", "MESSAGE_EXT_EMOJI"),
+    VIEW_GUILD_INSIGHTS(2L << 18, "VIEW_GUILD_INSIGHTS"),
+    CONNECT(2L << 19, "CONNECT", "VOICE_CONNECT"),
+    SPEAK(2L << 20, "SPEAK", "VOICE_SPEAK"),
+    MUTE_MEMBERS(2L << 21, "MUTE_MEMBERS", "VOICE_MUTE_OTHERS"),
+    DEAFEN_MEMBERS(2L << 22, "DEAFEN_MEMBERS", "VOICE_DEAF_OTHERS"),
+    MOVE_MEMBERS(2L << 23, "MOVE_MEMBERS", "VOICE_MOVE_OTHERS"),
+    USE_VAD(2L << 24, "USE_VAD", "VOICE_USE_VAD"),
+    CHANGE_NICKNAME(2L << 25, "CHANGE_NICKNAME", "NICKNAME_CHANGE"),
+    MANAGE_NICKNAMES(2L << 26, "MANAGE_NICKNAMES", "NICKNAME_MANAGE"),
+    MANAGE_ROLES(2L << 27, "MANAGE_ROLES", "MANAGE_PERMISSIONS"),
+    MANAGE_WEBHOOKS(2L << 28, "MANAGE_WEBHOOKS"),
+    MANAGE_GUILD_EXPRESSIONS(2L << 29, "MANAGE_GUILD_EXPRESSIONS"),
+    USE_APPLICATION_COMMANDS(2L << 30, "USE_APPLICATION_COMMANDS"),
+    REQUEST_TO_SPEAK(2L << 31, "REQUEST_TO_SPEAK"),
+    MANAGE_EVENTS(2L << 32, "MANAGE_EVENTS"),
+    MANAGE_THREADS(2L << 33, "MANAGE_THREADS"),
+    CREATE_PUBLIC_THREADS(2L << 34, "CREATE_PUBLIC_THREADS"),
+    CREATE_PRIVATE_THREADS(2L << 35, "CREATE_PRIVATE_THREADS"),
+    USE_EXTERNAL_STICKERS(2L << 36, "USE_EXTERNAL_STICKERS", "MESSAGE_EXT_STICKER"),
+    SEND_MESSAGES_IN_THREADS(2L << 37, "SEND_MESSAGES_IN_THREADS", "MESSAGE_SEND_IN_THREADS"),
+    USE_EMBEDDED_ACTIVITIES(2L << 38, "USE_EMBEDDED_ACTIVITIES"),
+    MODERATE_MEMBERS(2L << 39, "MODERATE_MEMBERS"),
+    VIEW_CREATOR_MONETIZATION_ANALYTICS(2L << 40, "VIEW_CREATOR_MONETIZATION_ANALYTICS"),
+    USE_SOUNDBOARD(2L << 41, "USE_SOUNDBOARD", "VOICE_USE_SOUNDBOARD"),
+    CREATE_GUILD_EXPRESSIONS(2L << 42, "CREATE_GUILD_EXPRESSIONS"),
+    CREATE_EVENTS(2L << 43, "CREATE_EVENTS", "CREATE_SCHEDULED_EVENTS"),
+    USE_EXTERNAL_SOUNDS(2L << 44, "USE_EXTERNAL_SOUNDS", "VOICE_USE_EXTERNAL_SOUNDS"),
+    SEND_VOICE_MESSAGES(2L << 45, "SEND_VOICE_MESSAGES", "MESSAGE_ATTACH_VOICE_MESSAGE"),
+    SET_VOICE_CHANNEL_STATUS(2L << 46, "SET_VOICE_CHANNEL_STATUS", "VOICE_SET_STATUS"),
+    SEND_POLLS(2L << 47, "SEND_POLLS", "MESSAGE_SEND_POLLS"),
+    USE_EXTERNAL_APPS(2L << 48, "USE_EXTERNAL_APPS", "USE_EXTERNAL_APPLICATIONS"),
+    PIN_MESSAGES(2L << 49, "PIN_MESSAGES"),
+    BYPASS_SLOWMODE(2L << 50, "BYPASS_SLOWMODE");
+
     private static final Logger logger = LoggerFactory.getLogger("ComitasBotJ.API.Permission");
     final long permission;
-    final String name;
-    private Permission(long permission, String name) {
+    final String[] names;
+    Permission(long permission, String... names) {
         this.permission = permission;
-        this.name = name;
+        this.names = names;
     }
 
     public static Permission fromValue(String permission) {
-        return switch (permission) {
-            case "CREATE_INSTANT_INVITE" -> CREATE_INSTANT_INVITE;
-            case "KICK_MEMBERS" -> KICK_MEMBERS;
-            case "BAN_MEMBERS" -> BAN_MEMBERS;
-            case "ADMINISTRATOR" -> ADMINISTRATOR;
-            case "MANAGE_CHANNEL" -> MANAGE_CHANNEL;
-            case "MANAGE_SERVER", "MANAGE_GUILD" -> MANAGE_GUILD;
-            case "ADD_REACTIONS", "MESSAGE_ADD_REACTION" -> ADD_REACTIONS;
-            case "VIEW_AUDIT_LOGS" -> VIEW_AUDIT_LOGS;
-            case "PRIORITY_SPEAKER" -> PRIORITY_SPEAKER;
-            case "STREAM", "VOICE_STREAM" -> STREAM;
-            case "VIEW_CHANNEL" -> VIEW_CHANNEL;
-            case "SEND_MESSAGES", "MESSAGE_SEND" -> SEND_MESSAGES;
-            case "SEND_TTS_MESSAGES", "MESSAGE_TTS" -> SEND_TTS_MESSAGES;
-            case "MANAGE_MESSAGES", "MESSAGE_MANAGE" -> MANAGE_MESSAGES;
-            case "EMBED_LINKS", "MESSAGE_EMBED_LINKS" -> EMBED_LINKS;
-            case "ATTACH_FILES", "MESSAGE_ATTACH_FILES" -> ATTACH_FILES;
-            case "READ_MESSAGE_HISTORY", "MESSAGE_HISTORY" -> READ_MESSAGE_HISTORY;
-            case "MENTION_EVERYONE", "MESSAGE_MENTION_EVERYONE" -> MENTION_EVERYONE;
-            case "USE_EXTERNAL_EMOJIS", "MESSAGE_EXT_EMOJI" -> USE_EXTERNAL_EMOJIS;
-            case "VIEW_GUILD_INSIGHTS" -> VIEW_GUILD_INSIGHTS;
-            case "CONNECT", "VOICE_CONNECT" -> CONNECT;
-            case "SPEAK", "VOICE_SPEAK" -> SPEAK;
-            case "MUTE_MEMBERS", "VOICE_MUTE_OTHERS" -> MUTE_MEMBERS;
-            case "DEAFEN_MEMBERS", "VOICE_DEAF_OTHERS" -> DEAFEN_MEMBERS;
-            case "MOVE_MEMBERS", "VOICE_MOVE_OTHERS" -> MOVE_MEMBERS;
-            case "USE_VAD", "VOICE_USE_VAD" -> USE_VAD;
-            case "CHANGE_NICKNAME", "NICKNAME_CHANGE" -> CHANGE_NICKNAME;
-            case "MANAGE_NICKNAMES", "NICKNAME_MANAGE" -> MANAGE_NICKNAMES;
-            case "MANAGE_ROLES", "MANAGE_PERMISSIONS" -> MANAGE_ROLES;
-            case "MANAGE_WEBHOOKS" -> MANAGE_WEBHOOKS;
-            case "MANAGE_GUILD_EXPRESSIONS" -> MANAGE_GUILD_EXPRESSIONS;
-            case "USE_APPLICATION_COMMANDS" -> USE_APPLICATION_COMMANDS;
-            case "REQUEST_TO_SPEAK" -> REQUEST_TO_SPEAK;
-            case "MANAGE_EVENTS" -> MANAGE_EVENTS;
-            case "MANAGE_THREADS" -> MANAGE_THREADS;
-            case "CREATE_PUBLIC_THREADS" -> CREATE_PUBLIC_THREADS;
-            case "CREATE_PRIVATE_THREADS" -> CREATE_PRIVATE_THREADS;
-            case "USE_EXTERNAL_STICKERS", "MESSAGE_EXT_STICKER" -> USE_EXTERNAL_STICKERS;
-            case "SEND_MESSAGES_IN_THREADS", "MESSAGE_SEND_IN_THREADS" -> SEND_MESSAGES_IN_THREADS;
-            case "USE_EMBEDDED_ACTIVITIES" -> USE_EMBEDDED_ACTIVITIES;
-            case "MODERATE_MEMBERS" -> MODERATE_MEMBERS;
-            case "VIEW_CREATOR_MONETIZATION_ANALYTICS" -> VIEW_CREATOR_MONETIZATION_ANALYTICS;
-            case "USE_SOUNDBOARD", "VOICE_USE_SOUNDBOARD" -> USE_SOUNDBOARD;
-            case "CREATE_GUILD_EXPRESSIONS" -> CREATE_GUILD_EXPRESSIONS;
-            case "CREATE_EVENTS", "CREATE_SCHEDULED_EVENTS" -> CREATE_EVENTS;
-            case "USE_EXTERNAL_SOUNDS", "VOICE_USE_EXTERNAL_SOUNDS" -> USE_EXTERNAL_SOUNDS;
-            case "SEND_VOICE_MESSAGES", "MESSAGE_ATTACH_VOICE_MESSAGE" -> SEND_VOICE_MESSAGES;
-            case "SET_VOICE_CHANNEL_STATUS", "VOICE_SET_STATUS" -> SET_VOICE_CHANNEL_STATUS;
-            case "SEND_POLLS", "MESSAGE_SEND_POLLS" -> SEND_POLLS;
-            case "USE_EXTERNAL_APPS", "USE_EXTERNAL_APPLICATIONS" -> USE_EXTERNAL_APPS;
-            case "PIN_MESSAGES" -> PIN_MESSAGES;
-            case "BYPASS_SLOWMODE" -> BYPASS_SLOWMODE;
-            default -> {
-                logger.warn("Unexpected value: {}", permission);
-                yield null;
-            }
-        };
+        return Arrays.stream(values())
+                .filter(e -> Arrays.asList(e.names).contains(permission))
+                .findFirst()
+                .orElseGet(() -> {
+                    logger.warn("Unexpected value: {}", permission);
+                    return null;
+                });
+    }
+
+    public static Long asLong(Permission[] permissions) {
+        return Arrays.stream(permissions).mapToLong(e -> e.permission).sum();
+    }
+
+    public static Permission[] fromLong(long permission) {
+        return Arrays.stream(values())
+                .filter(e -> (permission & e.permission) != 0)
+                .toArray(Permission[]::new);
     }
 }
